1101c21c0dbcfe7c5534e29d88971561c5692906
moeenn <moeen.v8@gmail.com>
2022-04-20 20:40:10 +0500

    optimized memory allocations

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "./libs/args-parser/ArgsParser.hpp"
 
+uint32_t allocations = 0;
+
+/**
+ *  track heap allocations
+ */
+void *operator new(size_t size) {
+	allocations++;
+	std::cout << "[alloc] " << allocations << '\n';
+	return malloc(size);
+}
+
 int main(int argc, char *argv[]) {
   ArgsParser parser{argc, argv};
 
-  std::string x;
-  std::string year;
+  const char* x;
+  const char* year;
   {
     parser.flag("-x", "String argument X", &x);
     parser.flag("--year", "String argument: Year", &year);
